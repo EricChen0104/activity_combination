f8dfa14f5e2dcdb40da9886aff66fa5b7446aab5
Eric Chen <eric@fakeemail.com>
2025-03-22 19:19:06 +0800

    fix connectDB

--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO);
+    mongoose.connect(
+      "mongodb+srv://vercel-admin-user:dWWcota0xcQ7XNRU@cluster0.luzul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
     // console.log(process.env.test);
     console.log("Mongodb is connected");
   } catch (err) {
